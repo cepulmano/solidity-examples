13323dcf8c02a8b800479f817a1619388988336a
cepulmano <43770710+cepulmano@users.noreply.github.com>
2023-10-27 09:19:48 +0800

    Update ConferenceRegistration.sol

--- ConferenceRegistration.sol
+++ ConferenceRegistration.sol
@@ -1,115 +1,69 @@
-// SPDX-License-Identifier: GPL-3.0 
-pragma solidity ^0.8.17 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
 
-/** 
- * @title ConferenceRegistration
- * @dev Implement a simple registration contract for a conference
- */
-contract ConferenceRegistration {
+contract PropertySystem {
 
-    address public organizer; // Address of rganizer of conference
+    address public administrator;
 
-    uint public maxParticipants; // Maximum no of participants
-    uint public noRegistrations; // Current number of registrations
-    uint public registrationPrice; // Price of registration
-    
-    enum STATUS { PAID, REFUNDED } // Status of registration
-    
-    // Store participant details
-    struct Participant {
-        STATUS status;
-        uint payment;
+    enum Status { ForSale, Sold }
+
+    struct Property {
+        address propertyOwner;
+        uint propertyPrice; // Ether
+        Status status;
     }
-    
-    // Mapping of registrant address and details
-    mapping(address => Participant) public registrations; 
 
-    /*
-     * Check if transaction sender is the organizer
-     */
-    modifier onlyOrganizer() {
-        require(
-            msg.sender == organizer,
-            "You have no access!"
-        );
+    mapping(uint => Property) properties; 
+
+    modifier onlyAdmin() {
+        require(msg.sender == administrator, "You are not the admin!");
         _;
     }
 
-    /*
-     * Check if maximum number of registrations is already reached
-     */
-    modifier maxReached() {
-        require(
-            noRegistrations < maxParticipants,
-            "Max number of registrations reached!"
-        );
+    modifier enoughPayment(uint propertyId) {
+        require(msg.value >= (properties[propertyId].propertyPrice * 1 ether), "Not enough payment");
         _;
     }
 
-    /*
-     * Check if the person provided enough payment
-     */
-    modifier enoughPayment() {
-        require(
-            msg.value >= registrationPrice,
-            "Payment is not enough!"
-        );
-        _;
-    }
-    
-    /*
-     * Check if transaction sender is already registered
-     */
-    modifier registered() {
-        require(
-            registrations[msg.sender].status == STATUS.PAID 
-                && registrations[msg.sender].payment == registrationPrice,
-            "You are not yet registered!"
-        );
+    modifier notOwner(uint propertyId) {
+        require (msg.sender != properties[propertyId].propertyOwner, "You are already the owner!");
         _;
     }
 
-    /*
-     * Constructor function
-     * @param _maxParticipants Maximum number of participants for this conference
-     * @param _regPrice Registration price set by the organizer
-     */
-    constructor(uint _maxParticipants, uint _regPrice) {
-        /*
-         * Assign constructor arguments to state variables
-         */
-        organizer = msg.sender;
-        maxParticipants = _maxParticipants;
-        registrationPrice = _regPrice * 1 ether;
+    modifier forSale(uint propertyId) {
+        require (Status.ForSale == properties[propertyId].status, "Propery not for sale!");
+        _;       
     }
-    
-    /*
-     * Register for the conference
-     */
-    function register() external payable maxReached enoughPayment {
-        registrations[msg.sender] = Participant({status: STATUS.PAID, payment: msg.value}); // Create a new participant object then add to registration mapping
-        noRegistrations++; // Increment the registrations
+
+    modifier enoughBalance(uint amount){
+        require ((amount * 1 ether) <= address(this).balance, "Not enough balance!");
+        _;
     }
-    
-    /*
-     * Refund your payment
-     */
-    function refund() external registered {
-        uint initialBalance = registrations[msg.sender].payment; // Assign current balance to local variable
-        
-        registrations[msg.sender].status = STATUS.REFUNDED; // Update status from PAID to REFUNDED
-        registrations[msg.sender].payment = 0; // Update payment set to 0
-        noRegistrations--; // Decrement the registrations
-        
-        address payable recipient = payable(msg.sender); // Cast the recipeient address to payable
-        recipient.transfer(initialBalance); // Transfer the balance to the recipient
+
+    constructor() {
+        administrator = msg.sender;
     }
-    
-    /*
-     * Get the total collection from registrations
-     */
-    function getCollection() public view onlyOrganizer returns(uint) {
-        return address(this).balance; // Return the balance of the contract
+
+    function addProperty(uint propertyId, uint propertyPrice) external onlyAdmin {
+        properties[propertyId] = Property(msg.sender,propertyPrice,Status.ForSale);
+    }
+
+    function buyProperty(uint propertyId) external payable enoughPayment(propertyId) notOwner(propertyId) forSale(propertyId) {
+        properties[propertyId].propertyOwner = msg.sender;
+        properties[propertyId].status = Status.Sold;
+    }
+
+    function viewProperty(uint propertyId) external view returns(Property memory) {
+        return properties[propertyId];
+    }
+
+    function getBalance() external view onlyAdmin returns(uint) {
+        return address(this).balance;
+    }
+
+    function withdraw(uint amount) external onlyAdmin enoughBalance(amount) {
+        address payable recipient = payable(msg.sender);
+        recipient.transfer(amount * 1 ether);
     }
 
 }
